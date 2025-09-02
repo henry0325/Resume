da830b73df4b103bc2fb714c9de192eb6401ab2e
henry0325 <164650678+henry0325@users.noreply.github.com>
2025-09-02 11:24:02 +0800

    Create Education.jsx

---
+++ src/components/Education.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Education() {
+  return (
+    <section id="education" className="resume-section">
+      <h2>Education</h2>
+      <h3 className="mb-0">Ming Chi University of Technology</h3>
+      <div className="subheading">Master in Mechanical Engineering, 2018</div>
+    </section>
+  );
+}
